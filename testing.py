9fa353558f82a09eae8b0854abc47781ee1c036f
David Thompson <49345822+davidkt99@users.noreply.github.com>
2021-03-07 05:05:17 -0600

    Add files via upload
    testing file contains code to interface with google calendar

---
+++ testing.py
@@ -0,0 +1,63 @@
+import datetime
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from datetime import datetime, timedelta
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar']
+
+CREDENTIALS_FILE = 'credentials.json'
+
+def get_calendar_service():
+   creds = None
+   # The file token.pickle stores the user's access and refresh tokens, and is
+   # created automatically when the authorization flow completes for the first
+   # time.
+   if os.path.exists('token.pickle'):
+       with open('token.pickle', 'rb') as token:
+           creds = pickle.load(token)
+   # If there are no (valid) credentials available, let the user log in.
+   if not creds or not creds.valid:
+       if creds and creds.expired and creds.refresh_token:
+           creds.refresh(Request())
+       else:
+           flow = InstalledAppFlow.from_client_secrets_file(
+               CREDENTIALS_FILE, SCOPES)
+           creds = flow.run_local_server(port=0)
+
+       # Save the credentials for the next run
+       with open('token.pickle', 'wb') as token:
+           pickle.dump(creds, token)
+
+   service = build('calendar', 'v3', credentials=creds)
+   return service
+
+
+
+
+def Add_Event(event_name, event_start, event_end):
+   # creates one hour event tomorrow 10 AM IST
+   service = get_calendar_service()
+
+   d = datetime.now().date()
+   tomorrow = datetime(d.year, d.month, d.day, 10)+timedelta(days=1)
+   start = tomorrow.isoformat()
+   end = (tomorrow + timedelta(hours=1)).isoformat()
+
+   event_result = service.events().insert(calendarId='primary',
+       body={
+           "summary": event_name,
+           "description": '',
+           "start": {"dateTime": event_start, "timeZone": 'US/Central'},
+           "end": {"dateTime": event_end, "timeZone": 'US/Central'},
+       }
+   ).execute()
+
+   print("created event")
+   print("id: ", event_result['id'])
+   print("summary: ", event_result['summary'])
+   print("starts at: ", event_result['start']['dateTime'])
+   print("ends at: ", event_result['end']['dateTime'])
